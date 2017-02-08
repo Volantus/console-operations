d7934c8601f2f40cf2efd7ceb239baf856c2dea7
Marius Meissner <info@marius-meissner.net>
2017-02-08 21:43:49 +0100

    Initial Commit

---
+++ src/Output/OutputOperations.php
@@ -0,0 +1,80 @@
+<?php
+namespace MMIT\ConsoleOperations\Output;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class OutputOperations
+ * @package Volante\SkyBukkit\RelayServer\Src\CLI
+ */
+trait OutputOperations
+{
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var boolean
+     */
+    protected $debugEnabled;
+
+    /**
+     * @param string $topic
+     * @param string $message
+     */
+    protected function writeInfoLine(string $topic, string $message)
+    {
+        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] ' . $message);
+    }
+
+    /**
+     * @param string $topic
+     * @param string $message
+     */
+    protected function writeDebugLine(string $topic, string $message)
+    {
+        if ($this->debugEnabled == null) {
+            $this->debugEnabled = getenv('OUTPUT_LEVEL') === 'DEBUG';
+        }
+
+        if ($this->debugEnabled) {
+            $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] ' . $message);
+        }
+    }
+
+    /**
+     * @param string $topic
+     * @param string $message
+     */
+    protected function writeGreenLine(string $topic, string $message)
+    {
+        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] <fg=green>' . $message . '</>');
+    }
+
+    /**
+     * @param string $topic
+     * @param string $message
+     */
+    protected function writeRedLine(string $topic, string $message)
+    {
+        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] <fg=red>' . $message . '</>');
+    }
+
+    /**
+     * @param string $topic
+     * @param string $message
+     */
+    protected function writeErrorLine(string $topic, string $message)
+    {
+        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] <error>' . $message . '</error>');
+    }
+
+    /**
+     * @return string
+     */
+    protected function currentTime() : string
+    {
+        return Carbon::now()->format('Y-m-d H:i:s');
+    }
+}
